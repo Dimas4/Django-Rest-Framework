4546dd885339fa9f2887cc655f56a91dc80250ad
Ivan <ivanshatukho@yandex.ru>
2018-12-10 15:39:10 +0300

    Improved SalaryCache test

--- api/tests.py
+++ api/tests.py
@@ -11,38 +11,29 @@ from celery_tasks.tasks import add_to_salary_cached
 
 class AnimalTestCase(TestCase):
     def setUp(self):
-        ivan = Person.objects.create(first_name="Ivan", second_name="Ivan")
-        andrey = Person.objects.create(first_name="Andrey", second_name="Andrey")
+        nikita = Person.objects.create(first_name="Nikita", second_name="Nikita")
 
         itechart = Company.objects.create(name='iTechArt', description='iTechArt is ...')
 
-        ivan_work_start_dt = datetime.strptime("2018-08-01", '%Y-%m-%d')
-        self.itechart_ivan = CompanyEmployee.objects.create(company=itechart, employee=ivan,
-                                                            work_start_dt=ivan_work_start_dt)
+        nikita_work_start_dt = datetime.strptime("2016-05-05", '%Y-%m-%d')
+        self.itechart_nikita = CompanyEmployee.objects.create(company=itechart, employee=nikita,
+                                                              work_start_dt=nikita_work_start_dt)
+        date = nikita_work_start_dt
+        self.nikita_annual_salary = {}
 
-        andrey_work_start_dt = datetime.strptime("2016-05-07", '%Y-%m-%d')
-        self.itechart_andrey = CompanyEmployee.objects.create(company=itechart, employee=andrey,
-                                                              work_start_dt=andrey_work_start_dt)
-
-        date = ivan_work_start_dt
-        self.ivan_annual_salary = 0
         while date <= datetime.now():
             salary = random.randint(100, 150)
-            Salary.objects.create(company_employee=self.itechart_ivan, salary=salary, date=date)
-            self.ivan_annual_salary += salary
-            date += relativedelta(months=1)
-
-        date = andrey_work_start_dt
-        self.andrey_annual_salary = 0
-        while date <= datetime.now():
-            salary = random.randint(400, 450)
-            Salary.objects.create(company_employee=self.itechart_andrey, salary=salary, date=date)
-            self.andrey_annual_salary += salary
+            Salary.objects.create(company_employee=self.itechart_nikita, salary=salary, date=date)
+            self.nikita_annual_salary[date.year] = 0 if not self.nikita_annual_salary.get(date.year) \
+                else self.nikita_annual_salary[date.year]
+            self.nikita_annual_salary[date.year] += salary
             date += relativedelta(months=1)
 
     @override_settings(CELERY_ALWAYS_EAGER=True)
     def test_salarycache(self):
-        year = 2018
-        add_to_salary_cached.delay(self.itechart_ivan.id, year)
-        self.assertEqual(self.ivan_annual_salary,
-                         SalaryCache.objects.get(company_employee=self.itechart_ivan, year=year).salary)
+        for year, salary in self.nikita_annual_salary.items():
+            add_to_salary_cached.delay(self.itechart_nikita.id, year)
+
+        for year, salary in self.nikita_annual_salary.items():
+            self.assertEqual(salary,
+                             SalaryCache.objects.get(company_employee=self.itechart_nikita, year=year).salary)
